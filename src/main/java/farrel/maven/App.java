2957c363361981c7a072f6ee7f38ebaf63735d4d
Farrel Putra <frl25ap@gmail.com>
2023-02-27 08:03:18 +0700

    add and implement dependency

--- src/main/java/farrel/maven/App.java
+++ src/main/java/farrel/maven/App.java
@@ -1,5 +1,7 @@
 package farrel.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        Gson gson = new Gson();
+
+        Person person = new Person("Farrel Athaillah Putra");
+        String json = gson.toJson(person); // convert object to json
+
+        System.out.println(json);
     }
 }
